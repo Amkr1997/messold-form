cdf705db4d83c929e4ff92f8b413c2a4a3359002
Aman Khare <khareaman754@gmail.com>
2025-05-23 15:54:31 +0530

    chore: updated db.config.js

--- server/db/db.connect.js
+++ server/db/db.connect.js
@@ -4,7 +4,12 @@ const mongo_uri = process.env.MONGO_URI;
 
 const initialisation = async () => {
   try {
-    const connectDB = await mongoose.connect(mongo_uri);
+    const connectDB = await mongoose.connect(mongo_uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // 5 seconds
+      socketTimeoutMS: 45000, // 45 seconds
+    });
 
     if (connectDB) {
       console.log("Connected to MongoDB");
