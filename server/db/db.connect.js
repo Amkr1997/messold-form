b18d94d305397d3e36d8858303abc05816132eb5
Aman Khare <khareaman754@gmail.com>
2025-05-31 00:35:04 +0530

    feat: modifed db

--- server/db/db.connect.js
+++ server/db/db.connect.js
@@ -3,10 +3,12 @@ require("dotenv").config({ path: ".env" });
 
 const initialisation = async () => {
   try {
-    const connectDB = await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000, // 5 seconds
-      socketTimeoutMS: 45000, // 45 seconds
-    });
+    // const connectDB = await mongoose.connect(process.env.MONGO_URI, {
+    //   serverSelectionTimeoutMS: 5000, // 5 seconds
+    //   socketTimeoutMS: 45000, // 45 seconds
+    // });
+
+    const connectDB = await mongoose.connect(process.env.MONGO_URI);
 
     if (connectDB) {
       console.log("Connected to MongoDB");
