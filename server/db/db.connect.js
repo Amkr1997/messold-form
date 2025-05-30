f2d074a9732093137532efb458e2f141a8d713cf
Aman Khare <khareaman754@gmail.com>
2025-05-31 00:05:15 +0530

    chore:modified db

--- server/db/db.connect.js
+++ server/db/db.connect.js
@@ -4,8 +4,6 @@ require("dotenv").config({ path: ".env" });
 const initialisation = async () => {
   try {
     const connectDB = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000, // 5 seconds
       socketTimeoutMS: 45000, // 45 seconds
     });
